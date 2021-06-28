2100753a0bb5f1d19d72487ce1224a396ba9ac93
ramano <ryota_amano@global.komatsu>
2021-06-28 13:00:17 +0900

    added muti thread process function in udp_sub_test

--- udp_sample/test/sub_udp.cpp
+++ udp_sample/test/sub_udp.cpp
@@ -8,64 +8,15 @@
 #include "simple_udp.h"
 
 #define BUFFER_SIZE  2048
-/*
-
-int main(void)
-{
-	//int sock;
-	int sock[2];
-	struct sockaddr_in addr;
-
-	char buf[BUFFER_SIZE];
-
-	//sock = socket(AF_INET, SOCK_DGRAM, 0);
-	sock[0] = socket(AF_INET, SOCK_DGRAM, 0);
-	sock[1] = socket(AF_INET, SOCK_DGRAM, 0);
-
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
-	addr.sin_addr.s_addr = INADDR_ANY;
-
-	//bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-	bind(sock[0], (struct sockaddr *)&addr, sizeof(addr));
-
-	addr.sin_port = htons(12346);
-	//bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-	bind(sock[1], (struct sockaddr *)&addr, sizeof(addr));
-
-
-	memset(buf, 0, sizeof(buf));
-	//recv(sock,buf, sizeof(buf), 0);
-	recv(sock[0],buf, sizeof(buf), 0);
-	printf("sock1:%s\n",buf);
-	memset(buf, 0, sizeof(buf));
-	//recv(sock,buf, sizeof(buf), 0);
-	recv(sock[1],buf, sizeof(buf), 0);
-	printf("sock2:%s\n",buf);
-
-
-
-	//close(sock);
-	close(sock[0]);
-	close(sock[1]);
-
-	return(0);
-
-}
-*/
 
 #include <thread>
+#include <mutex>
 
-void ThreadFunc1( struct sockaddr_in addr)
+bool buffer_full_flag_;
+
+// Main Thread
+void ThreadFunc1( void )
 {
-	int sock;
-
-	char buf[BUFFER_SIZE];
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-
-	bind(sock, (struct sockaddr *)&addr, sizeof(addr));
-
-	int counter = 0;
 	while(1)
 	{
 		memset( buf, 0, sizeof(buf));
@@ -103,6 +54,8 @@ int main(void)
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(12345);
 	addr.sin_addr.s_addr = INADDR_ANY;
+
+	buffer_full_flag_ = false;
 
 	std::thread th1(ThreadFunc1, addr);
 	th1.join();
--- udp_sample/test/udp_sub_test.cpp
+++ udp_sample/test/udp_sub_test.cpp
@@ -17,41 +17,33 @@
 
 #include <chrono>
 #include <thread>
+#include <mutex>
 
 #define BUFFER_SIZE  2048
 
-int main(void)
+#define POINT_BUFFER_LIMIT 100
+#define SINGLE_POINT_DATA_SIZE 4
+
+std::mutex mtx_;
+bool data_set_flag_ = false;
+uint8_t buf_[BUFFER_SIZE];
+
+void ReceivePacket(struct sockaddr_in addr)
 {
-	int sock;
-	struct sockaddr_in addr;
-
-	//sock = socket(AF_INET, SOCK_DGRAM, 0);
-	sock = socket(AF_INET, SOCK_DGRAM, 0);
-
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(12345);
-	addr.sin_addr.s_addr = INADDR_ANY;
-
-	//bind(sock, (struct sockaddr *)&addr, sizeof(addr));
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
 	bind(sock, (struct sockaddr *)&addr, sizeof(addr));
 
 	//char buf[BUFFER_SIZE];
 	uint8_t tmp_buf[BUFFER_SIZE];
-	uint8_t buf[BUFFER_SIZE];
-	uint8_t* p = buf;
+	uint8_t* p = tmp_buf;
 
 	int point_num = 0;
-	int point_limit = 100;
-	int point_size = 4;
+	int point_limit = POINT_BUFFER_LIMIT;
+	int point_size = SINGLE_POINT_DATA_SIZE;
 	int point_id = 1;
-	bool data_set_flag = false;
-
-
-	//int id, x, y, z;
 
     while(1)
     {
-	    memset(buf, 0, sizeof(tmp_buf));
 	    recv( sock, tmp_buf, sizeof(tmp_buf), 0);
 		//id = tmp_buf[0];
 		//x = tmp_buf[1];
@@ -64,32 +56,95 @@ int main(void)
 
 		if(point_num > point_limit)
 		{
-			memset(buf, 0 , sizeof(buf));
-			memcpy(buf, tmp_buf, point_num * point_size);
-			data_set_flag = true;
-
-			printf("%d of points are set!! %d th point is (%d, %d, %d, %d)\n", 
-					point_num,
-					point_id,
-					buf[0 + point_size * point_id], 
-					buf[1 + point_size * point_id], 
-					buf[2 + point_size * point_id], 
-					buf[3 + point_size * point_id]);
-
+			printf("Receive Func:PointLimit\n");
+			std::lock_guard<std::mutex> lock(mtx_);
+			memset(buf_, 0 , sizeof(buf_));
+			memcpy(buf_, tmp_buf, point_num * point_size);
+	    	memset(tmp_buf, 0, sizeof(tmp_buf));
 			p = tmp_buf;
 			point_num = 0;
+			data_set_flag_ = true;
 		}
-
-
-
-
-	    //printf("sock1:%s\n",buf);
 
     }
 
-
     close(sock);
 
+}
+
+void PointPurseMain( int hz )
+{
+	std::chrono::system_clock::time_point start, now;
+	start = std::chrono::system_clock::now();
+	now = std::chrono::system_clock::now();
+	double now_process_time = std::chrono::duration_cast<std::chrono::milliseconds>(now-start).count();// to milliseconds
+
+	double process_target_time = 1000 / (double)hz;
+	int process_time = 0;
+
+	int point_num = POINT_BUFFER_LIMIT;
+	int point_size = SINGLE_POINT_DATA_SIZE;
+	int point_id;
+
+	while(1)
+	{
+
+		std::lock_guard<std::mutex> lock(mtx_);
+		if(data_set_flag_)
+		{
+
+			for(int i=0; i<point_num; i++)
+			{
+				point_id = i;
+				printf("Main Thread:%d of points are set!! %d th point is (%d, %d, %d, %d)\n", 
+						point_num,
+						point_id,
+						buf_[0 + point_size * point_id], 
+						buf_[1 + point_size * point_id], 
+						buf_[2 + point_size * point_id], 
+						buf_[3 + point_size * point_id]);
+
+
+			}
+			data_set_flag_ = false;
+		}
+
+		now = std::chrono::system_clock::now();
+		now_process_time = std::chrono::duration_cast<std::chrono::milliseconds>(now-start).count();
+		process_time = (int)(process_target_time - now_process_time);
+
+		//printf("Process Time:%d\n", process_time);
+
+		if(process_time > 0)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(process_time));
+			start = std::chrono::system_clock::now();
+		}
+		
+
+	}
+
+}
+
+int main(void)
+{
+	//int sock;
+	struct sockaddr_in addr;
+
+	//sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(12345);
+	addr.sin_addr.s_addr = INADDR_ANY;
+
+
+	std::thread th1(ReceivePacket,addr);
+	std::thread th2(PointPurseMain, 20);
+	th1.join();
+	th2.join();
+
+	//ReceivePacket(addr);
+	//int id, x, y, z;
     return(0);
 
 }
